b4541b51d8cf3607774066f5a2f45eefeaf0d483
DavidPythonstar <davidpainzug@gmail.com>
2023-12-07 20:43:13 +0300

    all operator complete

--- statics/script.js
+++ statics/script.js
@@ -7,77 +7,72 @@ let num2 = 0;
 let result = 0;
 let operators = document.querySelectorAll('.operator')
 let operator = ''
-let equal = 
+let equalButton = document.querySelector('.equal');
 
 // Clearing the screen
-clearButton.addEventListener('click', ()=>
-{
+clearButton.addEventListener('click', () => {
     screen.textContent = ""
+    num1 = 0;
+    num2 = 0;
+    result = 0;
 })
 
 // Getting the clicked button
 numberButtons.forEach((button) => {
     button.addEventListener('click', (event) => {
         clicked = event.target.textContent;
-        screen.textContent += clicked
-        num1 = 0
-        num2 = 0;
-        result = 0;
-
+        screen.textContent += clicked;
+        if (operator === '') {
+            num1 = parseFloat(screen.textContent);
+        } else {
+            num2 = parseFloat(screen.textContent);
+        }
     })
 })
 
-//creating the arithmetic functions
-function add(number1, number2)
-{
-    sum = number1 + number2;
-    return sum
+// Creating the arithmetic functions
+function add(number1, number2) {
+    return number1 + number2;
 }
 
-function divide(number1,number2)
-{
-    result = number1/number2;
-    return result;
+function divide(number1, number2) {
+    return number1 / number2;
 }
 
-
-function mult(number1,number2)
-{
-    result = number1*number2;
-    return result;
+function mult(number1, number2) {
+    return number1 * number2;
 }
 
-function sub(number1,number2)
-{
-    result = number1-number2;
-    return result;
+function sub(number1, number2) {
+    return number1 - number2;
 }
 
-
-function operate(operator,num1,num2)
-{
+function operate(operator, num1, num2) {
     if (operator === '+')
-        result = add(num1,num2);
+        return add(num1, num2);
     else if (operator === '-')
-        result = sub(num1, num2);
+        return sub(num1, num2);
     else if (operator === '*')
-        result = mult(num1, num2);
+        return mult(num1, num2);
     else if (operator === '/')
-        result = divide(num1,num2);
+        return divide(num1, num2);
 }
 
-
-operators.forEach((button) =>
-{
-    button.addEventListener('click', (button)=>
-    {
-        operator = button.target.textContent
-        
-
+// Handling operator button click
+operators.forEach((button) => {
+    button.addEventListener('click', (button) => {
+        operator = button.target.textContent;
+        num1 = parseFloat(screen.textContent);
+        screen.textContent = "";
     })
-
 })
 
-
-
-
+// Handling equal button click
+equalButton.addEventListener('click', () => {
+    num2 = parseFloat(screen.textContent);
+    result = operate(operator, num1, num2);
+    screen.textContent = result;
+    num1 = result;
+    num2 = 0;
+    operator = '';
+})
